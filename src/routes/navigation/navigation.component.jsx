f51424579fbf5f67dcb84dc55fa7be38c514ef1a
batel shriki <batel2610@gmail.com>
2022-09-02 17:26:07 +0300

    sign-up & sign-in

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -18,7 +18,7 @@ const Navigation = () => {
             SHOP
             </Link>
             
-            <Link className="nav-link" to="/signIn">
+            <Link className="nav-link" to="/auth">
             SignIn
             </Link>
           </div>
